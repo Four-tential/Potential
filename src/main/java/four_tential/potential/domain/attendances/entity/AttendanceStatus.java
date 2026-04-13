1b75b12eb049d44e9610b7d51b3f12493bcf8f46
김대훈 <eogns136@gmail.com>
2026-04-13 19:27:34 +0900

    fix: AttendanceStatus descriptionn 작성

--- src/main/java/four_tential/potential/domain/attendances/entity/AttendanceStatus.java
+++ src/main/java/four_tential/potential/domain/attendances/entity/AttendanceStatus.java
@@ -1,6 +1,14 @@
 package four_tential.potential.domain.attendances.entity;
 
+import lombok.Getter;
+
+@Getter
 public enum AttendanceStatus {
-    ATTEND,
-    ABSENT
+    ATTEND("출석"),
+    ABSENT("결석");
+
+    private final String description;
+    AttendanceStatus(String description) {
+        this.description = description;
+    }
 }
