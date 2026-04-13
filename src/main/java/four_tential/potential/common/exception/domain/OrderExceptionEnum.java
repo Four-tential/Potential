f5872930fb9afd70bcb6cf96153c3c11152e9385
김동진 <kimdj9790@naver.com>
2026-04-13 17:56:42 +0900

    feature: Entity, Status

---
+++ src/main/java/four_tential/potential/common/exception/domain/OrderExceptionEnum.java
@@ -0,0 +1,26 @@
+package four_tential.potential.common.exception.domain;
+
+import four_tential.potential.common.exception.ServiceErrorCode;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public enum OrderExceptionEnum implements ServiceErrorCode {
+
+    ERR_NOT_FOUND_ORDER(HttpStatus.NOT_FOUND, "주문 정보를 찾을 수 없습니다"),
+    ERR_ALREADY_RESERVED(HttpStatus.CONFLICT, "동일한 시간대에 이미 예약된 코스가 있습니다"),
+    ERR_NO_AVAILABLE_SEATS(HttpStatus.BAD_REQUEST, "코스의 잔여석이 없습니다"),
+    ERR_INVALID_ORDER_STATUS(HttpStatus.BAD_REQUEST, "변경할 수 없는 주문 상태입니다"),
+    ERR_CANNOT_CANCEL_ORDER(HttpStatus.BAD_REQUEST, "코스 시작 7일 전까지만 취소가 가능합니다"),
+    ERR_ORDER_EXPIRED(HttpStatus.BAD_REQUEST, "결제 가능 시간이 초과되었습니다"),
+    ERR_QUEUE_FULL(HttpStatus.SERVICE_UNAVAILABLE, "현재 대기열이 가득 차서 신청할 수 없습니다")
+    ;
+
+    private final HttpStatus httpStatus;
+    private final String message;
+
+    OrderExceptionEnum(HttpStatus httpStatus, String message) {
+        this.httpStatus = httpStatus;
+        this.message = message;
+    }
+}
