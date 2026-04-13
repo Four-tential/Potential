b8da6380f3bd84f36b6ee9bc17a99d0d5bd7b504
Lee Hanbi <hanbi6717@naver.com>
2026-04-13 20:36:33 +0900

    feat: 환불 entity 및 repository 작성

---
+++ src/main/java/four_tential/potential/domain/payment/entity/Refund.java
@@ -0,0 +1,62 @@
+package four_tential.potential.domain.payment.entity;
+
+import four_tential.potential.domain.payment.enums.RefundReason;
+import four_tential.potential.domain.payment.enums.RefundStatus;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.UuidGenerator;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Entity
+@Table(name = "refunds")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Refund {
+
+    @Id
+    @UuidGenerator(style = UuidGenerator.Style.VERSION_7)
+    @Column(nullable = false, updatable = false, columnDefinition = "BINARY(16)")
+    private UUID id;
+
+    @Column(name = "payment_id", nullable = false, columnDefinition = "BINARY(16)")
+    private UUID paymentId;
+
+    @Column(name = "refund_price", nullable = false)
+    private Long refundPrice;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 30)
+    private RefundReason reason;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 30)
+    private RefundStatus status;
+
+    @Column(name = "refunded_at")
+    private LocalDateTime refundedAt;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+    public static Refund create(
+            UUID paymentId,
+            Long refundPrice,
+            RefundReason reason) {
+        Refund refund = new Refund();
+        refund.paymentId = paymentId;
+        refund.refundPrice = refundPrice;
+        refund.reason = reason;
+        refund.status = RefundStatus.COMPLETED;
+        refund.refundedAt = LocalDateTime.now();
+        refund.createdAt = LocalDateTime.now();
+        refund.updatedAt = LocalDateTime.now();
+        return refund;
+    }
+}
