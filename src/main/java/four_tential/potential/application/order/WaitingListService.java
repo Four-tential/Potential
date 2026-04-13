c017f5f36ccfc372bc077f8a933bbe28d612079e
김동진 <kimdj9790@naver.com>
2026-04-13 18:17:25 +0900

    refactor: 필요없는 import 삭제

--- src/main/java/four_tential/potential/application/order/WaitingListService.java
+++ src/main/java/four_tential/potential/application/order/WaitingListService.java
@@ -3,7 +3,6 @@ package four_tential.potential.application.order;
 import four_tential.potential.domain.order.WaitingListRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
