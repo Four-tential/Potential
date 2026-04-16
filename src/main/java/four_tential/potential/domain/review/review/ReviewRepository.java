158b4a3f2a8221a9f3ae54f2ab2ba8ac0ab31c71
김대훈 <eogns136@gmail.com>
2026-04-16 14:03:24 +0900

    feat: review Repository 메서드 작성

--- src/main/java/four_tential/potential/domain/review/review/ReviewRepository.java
+++ src/main/java/four_tential/potential/domain/review/review/ReviewRepository.java
@@ -2,7 +2,15 @@ package four_tential.potential.domain.review.review;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface ReviewRepository extends JpaRepository<Review, UUID> {
+
+    List<Review> findAllByCourseId(UUID courseId);
+
+    Optional<Review> findByIdAndMemberId(UUID id, UUID memberId);
+
+    boolean existsByOrderIdAndMemberId(UUID orderId, UUID memberId);
 }
