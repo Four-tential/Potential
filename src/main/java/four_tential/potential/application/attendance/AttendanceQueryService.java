53407095034187d9529c28220de91b5707972023
김대훈 <eogns136@gmail.com>
2026-04-16 14:04:58 +0900

    refactor: dto 패키지 이동

--- src/main/java/four_tential/potential/application/attendance/AttendanceQueryService.java
+++ src/main/java/four_tential/potential/application/attendance/AttendanceQueryService.java
@@ -2,7 +2,7 @@ package four_tential.potential.application.attendance;
 
 import four_tential.potential.domain.attendance.Attendance;
 import four_tential.potential.domain.attendance.AttendanceRepository;
-import four_tential.potential.domain.attendance.dto.AttendanceListResponse;
+import four_tential.potential.presentation.attendance.dto.AttendanceListResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
