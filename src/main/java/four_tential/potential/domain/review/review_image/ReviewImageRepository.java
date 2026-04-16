d9837134ea8936615b0b28ddf22a46e2ae25e774
김대훈 <eogns136@gmail.com>
2026-04-16 14:05:30 +0900

    feat: review image repository 작성

--- src/main/java/four_tential/potential/domain/review/review_image/ReviewImageRepository.java
+++ src/main/java/four_tential/potential/domain/review/review_image/ReviewImageRepository.java
@@ -2,7 +2,11 @@ package four_tential.potential.domain.review.review_image;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface ReviewImageRepository extends JpaRepository<ReviewImage, UUID> {
+    void deleteAllByReviewId(UUID reviewId);
+
+    List<ReviewImage> findAllByReviewId(UUID id);
 }
