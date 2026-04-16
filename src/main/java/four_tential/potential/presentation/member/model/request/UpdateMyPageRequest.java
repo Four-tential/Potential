51a9505b48baa50e99de9e37e8743e3b619a66f5
KYJ <nananan1213@gmail.com>
2026-04-16 16:11:09 +0900

    refactor: 대용량 반복시 오버플로우 방지로 / 반복 패스 방지

--- src/main/java/four_tential/potential/presentation/member/model/request/UpdateMyPageRequest.java
+++ src/main/java/four_tential/potential/presentation/member/model/request/UpdateMyPageRequest.java
@@ -6,7 +6,7 @@ public record UpdateMyPageRequest(
         @Pattern(regexp = "^01[016789]-\\d{3,4}-\\d{4}$", message = "올바른 휴대전화 번호 형식으로 입력해주세요 (- 포함)")
         String phone,
 
-        @Pattern(regexp = "^https?://[\\da-z.-]+\\.[a-z]{2,6}(/[\\w.-]+)*/?$", message = "올바른 URL 형식으로 입력해주세요")
+        @Pattern(regexp = "^https?://[\\da-z.-]+\\.[a-z]{2,6}(/[\\w./-]*)?$", message = "올바른 URL 형식으로 입력해주세요")
         String profileImageUrl
 ) {
 }
